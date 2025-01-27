6c885e1ab318329525d031e5928a0cecde7e727e
Jan Max Meyer <jmm@phorward.de>
2025-01-27 21:35:39 +0100

    Update dependencies to latest stable state

--- deploy/render/__init__.py
+++ deploy/render/__init__.py
@@ -1,3 +1,4 @@
+from viur.core.render.html.utils import jinjaGlobalFunction
 from viur.core.render import admin, html, json, vi
 
 
@@ -15,43 +16,42 @@ def gen_postProcessAppObj(renderer):
 json._postProcessAppObj = gen_postProcessAppObj(json)
 
 
-import typing as t
-import json as _json
-import logging, requests
-from viur.core.render.html.utils import jinjaGlobalFunction
 @jinjaGlobalFunction
-def inject_vite(render, development: bool | None = None, development_server: str = "http://localhost:8081") -> t.Any:
+def inject_vite(render, development: bool | None = None, development_server: str = "http://localhost:8081"):
     """build vue imports from manifest"""
 
     if development is None:
+        import logging
+        import requests
+
         try:
-            resp = requests.get(development_server)
+            resp = requests.get(development_server, timeout=2)
             development = resp.status_code == 200
-        except Exception as e:
-           development = False
+        except Exception:
+            logging.warning(f"{development_server} isn't active, using prebuilt fallback")
+            development = False
 
     if development:
-        return f"""<script type="module" src="{development_server}/@vite/client"></script>
-                  <script type="module" src="{development_server}/main.js"></script>"""
+        return f"""
+            <script type="module" src="{development_server}/@vite/client"></script>
+            <script type="module" src="{development_server}/main.js"></script>
+        """
 
-    vite_path = "/static/site"
+    import json
+    vite_path = "static/site"
 
     try:
-        fd = open(f"static/site/.vite/manifest.json", "r")
-        manifest = _json.load(fd)
-    except:
+        fd = open(f"{vite_path}/.vite/manifest.json", "r")
+        manifest = json.load(fd)
+    except Exception as e:
         raise Exception(
-            f"Vite manifest file not found or invalid. Maybe your {vite_path}/.vite/manifest.json file is empty?"
-        )
-    imports_files = ""
-    if "imports" in manifest["index.html"]:
-        imports_files = "".join(
-            [
-                f'<script type="module" src="{vite_path}/{manifest[file]["file"]}"></script>'
-                for file in manifest["index.html"]["imports"]
-            ]
+            f"Vite manifest file not found or invalid {e=}. Maybe your /{vite_path}/.vite/manifest.json file is empty?"
         )
 
-    return f"""<script type="module" src="{vite_path}/{manifest['index.html']['file']}"></script>
-        <link rel="stylesheet" type="text/css" href="{vite_path}/{manifest['index.html']['css'][0]}" />
-        {imports_files}"""
+    return f"""
+        <script type="module" src="/{vite_path}/{manifest["index.html"]["file"]}"></script>
+        <link rel="stylesheet" type="text/css" href="/{vite_path}/{manifest["index.html"]["css"][0]}" />
+    """ + "".join(
+        f"""<script type="module" src="/{vite_path}/{manifest[file]["file"]}"></script>"""
+        for file in (manifest["index.html"].get("imports") or ())
+    )
